70cfa33034a1ba197c01a3af48a89b40565886b3
Ryan Pferrer <ryanpferrer@gmail.com>
2019-12-23 20:04:26 -0500

    added unittest skeleton file

---
+++ test_randomselector.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""Tests randomselector.py."""
+
+
+import unittest
+
+
+class test_randomselector(unittest.TestCase):
+    """Primary testing function."""
+
+
+if __name__ == "__main__":
+    unittest.main()
+    print("Everything passed!")
