d8bdac46a6dd5d91c6be33ad9cfbfa61e225ae74
Ryan Pferrer <ryanpferrer@gmail.com>
2019-12-27 15:35:43 -0500

    added import and skeleton functions

--- test_randomselector.py
+++ test_randomselector.py
@@ -3,10 +3,19 @@
 
 
 import unittest
+import randomselector
 
 
 class test_randomselector(unittest.TestCase):
     """Primary testing function."""
+
+    def setUpClass(cls):
+        """Set up the data that will be used in subsequent tests."""
+        cls._entrantsData = randomselector.buildEntrants(
+            "../../sample-files/github-repos/entry-drawer/entriesTestData.txt")
+
+    def test_removeWinner(cls):
+        """Test the removeWinner() function."""
 
 
 if __name__ == "__main__":
