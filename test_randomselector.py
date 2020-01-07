56d3181ddac6dc5d622b4c6cfc61f6f1f4fee890
Ryan Pferrer <ryanpferrer@gmail.com>
2020-01-06 20:23:38 -0500

    setupclass method implemented along with helper function

--- test_randomselector.py
+++ test_randomselector.py
@@ -2,22 +2,39 @@
 """Tests randomselector.py."""
 
 
+import csv
+import io
+import random
+import string
 import unittest
-import randomselector
 
 
 class test_randomselector(unittest.TestCase):
     """Primary testing function."""
 
-    def setUpClass(cls):
+    @classmethod
+    def setUpClass(self):
         """Set up the data that will be used in subsequent tests."""
-        cls._entrantsData = randomselector.buildEntrants(
-            "../../sample-files/github-repos/entry-drawer/entriesTestData.txt")
+        with io.StringIO() as output:
+            writer = csv.writer(output, delimiter=',')
+            writer.writerow(["entrant", "entries"])
+            # Set up loop to generate n number of csv lines
+            for x in range(10):
+                randName = generate_random_entry_name()
+                randEntries = random.randint(1, 300)
+                writer.writerow([str(randName), str(randEntries)])
+            csvFileContents = output.getvalue()
+        self.csvFile = open("testFile", "w")
+        self.csvFile.write(csvFileContents)
 
-    def test_removeWinner(cls):
+    def test_removeWinner(self):
         """Test the removeWinner() function."""
+
+
+def generate_random_entry_name(size=6, chars=string.ascii_uppercase + string.digits):
+    """Generate a random string that serves as an entrant name for testing."""
+    return ''.join(random.choice(chars) for x in range(size))
 
 
 if __name__ == "__main__":
     unittest.main()
-    print("Everything passed!")
