19e4f6df82402921f9d04827d4b19f2d506c3658
Ryan Pferrer <ryanpferrer@gmail.com>
2020-01-08 08:28:20 -0500

    hard coded test entrant names. wrote additional unit tests

--- test_randomselector.py
+++ test_randomselector.py
@@ -6,7 +6,7 @@ import csv
 import io
 import os
 import random
-from randomselector import buildEntrants
+import randomselector
 import string
 import unittest
 
@@ -21,20 +21,38 @@ class test_randomselector(unittest.TestCase):
             writer = csv.writer(output, delimiter=',')
             writer.writerow(["entrant", "entries"])
             # Set up loop to generate n number of csv lines
-            for x in range(10):
-                randName = generate_random_entry_name()
+            testEntrantNames = ['Bojack', 'Carolyn', 'Todd', 'Sarah', 'Peanutbutter', 'Diane',
+                                'Pinky', 'Herb', 'Butterscotch', 'Beatrice']
+            for name in testEntrantNames:
                 randEntries = random.randint(1, 300)
-                writer.writerow([str(randName), str(randEntries)])
+                writer.writerow([name, str(randEntries)])
             csvFileContents = output.getvalue()
         csvFile = open("testFile.csv", "w")
         with csvFile:
             csvFile.write(csvFileContents)
-        self.entrants = buildEntrants(fileName=csvFile)
+        self.entrants = randomselector.buildEntrants(fileName="testFile.csv")
 
     @classmethod
     def tearDownClass(self):
         """Remove created file."""
         os.remove("testFile.csv")
+
+    def test_isUnique(self):
+        """Test the isUnique() function."""
+        randKey = random.randint(1, len(self.entrants))
+        name = self.entrants[randKey]
+        self.assertTrue(randomselector.isUnique(name, self.entrants))
+
+    def test_selectWinner(self):
+        """Test the selectWinner() function."""
+        winner = randomselector.selectWinner(self.entrants)
+        self.assertTrue(winner in self.entrants)
+
+    def test_removeWinner(self):
+        """Test the removeWinner() function."""
+        randKey = random.randint(1, len(self.entrants))
+        randEntrant = self.entrants[randKey]
+        self.assertTrue(randEntrant not in randomselector.removeWinner(self.entrants, randEntrant))
 
 
 def generate_random_entry_name(size=6, chars=string.ascii_uppercase + string.digits):
